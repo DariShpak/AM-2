0eaa02023f23833402fbf3cadd9201bf11b90c9f
Daria Shpak <119341842+DariShpak@users.noreply.github.com>
2025-01-21 19:22:46 +0200

    Update section-scroll.js

--- js/section-scroll.js
+++ js/section-scroll.js
@@ -1,17 +1,29 @@
-
 gsap.registerPlugin(ScrollTrigger)
 
 const wrapper = document.querySelector(".services-wrapper")
 
+// gsap.to(wrapper, {
+//   x: () => -(wrapper.scrollWidth - window.innerWidth), // Рухаємо весь контейнер вліво
+//   ease: "none",
+//   scrollTrigger: {
+//     trigger: ".services-section", // Секція, де почнеться скрол
+//     start: "top top", // Початок анімації
+//     end: () => `+=${wrapper.scrollWidth}`, // Кінець анімації (ширина всього контейнера)
+//     scrub: true, // Плавність скролу
+//     pin: true, // Фіксуємо секцію під час горизонтального скролу
+//     anticipatePin: 1
+//   }
+// })
+
 gsap.to(wrapper, {
-  x: () => -(wrapper.scrollWidth - window.innerWidth), // Рухаємо весь контейнер вліво
+  x: () => -(wrapper.scrollWidth - window.innerWidth),
   ease: "none",
   scrollTrigger: {
-    trigger: ".services-section", // Секція, де почнеться скрол
-    start: "top top", // Початок анімації
-    end: () => `+=${wrapper.scrollWidth}`, // Кінець анімації (ширина всього контейнера)
-    scrub: true, // Плавність скролу
-    pin: true, // Фіксуємо секцію під час горизонтального скролу
-    anticipatePin: 1
+    trigger: ".services-section",
+    start: "top top",
+    end: () => `+=${wrapper.scrollWidth - window.innerWidth}`,
+    scrub: true,
+    pin: true,
+    anticipatePin: 1,
   }
 })
